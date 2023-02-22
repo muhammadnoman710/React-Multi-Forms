180ac8c642ddd1e787b6fd56aee0968cc64c02fe
muhammadnoman710 <nomi32655@gmail.com>
2023-02-22 17:58:01 +0500

    first commit

---
+++ Components/SideBar/SideBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './SideBar.css'
+function SideBar() {
+  return (
+<div>
+<div className="ui raised very padded text container segment">
+  <div className="ui raised very padded text container segment info"/>
+  <h2 className="ui header">Personal Info</h2>
+</div>
+</div>
+  )
+}
+
+export default SideBar
